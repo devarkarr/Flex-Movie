d2952b4e9e855b3e817949e8d7620c9a218382fe
christo4RY <arkarlin486@gmail.com>
2024-09-06 11:10:25 +0630

    fix circle

--- src/components/MovieCard.tsx
+++ src/components/MovieCard.tsx
@@ -1,4 +1,4 @@
-import { Box, Image, Text } from "@mantine/core";
+import { Box, Flex, Image, Text } from "@mantine/core";
 import VoteAverageCircle from "./VoteAverageCircle";
 import { MovieType } from "../store/server/trending/interface";
 import { TvSeriesType } from "../store/server/tv/interface";
@@ -19,13 +19,24 @@ const MovieCard = ({ movie }: MovieCardType) => {
           alt="flex-movie"
           fallbackSrc="https://i.pinimg.com/564x/17/96/f9/1796f9ed980412e3b9b267d3204ff7ee.jpg"
         />
-        <VoteAverageCircle rate={Number(movie.vote_average.toFixed(1))} />
+        <Flex
+          pos={"absolute"}
+          align={"center"}
+          justify={"center"}
+          bottom={"-1.2em"}
+          left={"0.8em"}
+        >
+          <VoteAverageCircle rate={Number(movie.vote_average.toFixed(1))} />
+        </Flex>
       </Box>
-        <Text style={{ textAlign: "center" }} fw={500} size={"16px"} mt={32}>
-      <Link to={`/movie/${movie.id}`} style={{color:'black',textDecoration:'none'}}>
+      <Text style={{ textAlign: "center" }} fw={500} size={"16px"} mt={32}>
+        <Link
+          to={`/movie/${movie.id}`}
+          style={{ color: "black", textDecoration: "none" }}
+        >
           {movie.title}
-      </Link>
-        </Text>
+        </Link>
+      </Text>
       <Text style={{ textAlign: "center" }} c="dimmed" size={"14px"} mt={8}>
         {movie.release_date}
       </Text>
--- src/components/VoteAverageCircle.tsx
+++ src/components/VoteAverageCircle.tsx
@@ -1,33 +1,29 @@
-import {  Flex } from "@mantine/core";
 import classes from "./styles/moviecard.module.css";
 import { CircularProgressbar } from "react-circular-progressbar";
 import "react-circular-progressbar/dist/styles.css";
 
-
 const VoteAverageCircle = ({ rate }: { rate: number }) => {
   return (
-    <Flex pos={'absolute'} align={'center'} justify={'center'} bottom={'-1.2em'} left={'0.8em'}>
-        <CircularProgressbar
-          className={classes.score}
-          styles={{
-            path: {
-              stroke: rate < 5 ? "red" : rate < 7 ? "orange" : "green",
-              strokeLinecap: "round",
-            },
-            text: {
-              fontSize: 27,
-            },
-            trail: {
-              stroke: '#335159',
-            },
-            background:{
-                background:'#081c22'
-            }
-          }}
-          value={rate}
-          text={`${rate}%`}
-        />
-    </Flex>
+    <CircularProgressbar
+      className={classes.score}
+      styles={{
+        path: {
+          stroke: rate < 5 ? "red" : rate < 7 ? "orange" : "green",
+          strokeLinecap: "round",
+        },
+        text: {
+          fontSize: 27,
+        },
+        trail: {
+          stroke: "#335159",
+        },
+        background: {
+          background: "#081c22",
+        },
+      }}
+      value={rate}
+      text={`${rate}%`}
+    />
   );
 };
 
